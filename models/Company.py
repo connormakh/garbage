9ab5f0f33759fdeb8baded71a081e87ddb01fba9
Connor <connormakhlouta@gmail.com>
2017-10-08 19:57:37 +0300

    initial commit

---
+++ models/Company.py
@@ -0,0 +1,31 @@
+from app import db
+
+class Company(db.Model):
+    """This Class represents the company table, used for the user type company, in the admin portal"""
+
+    __tablename__ = 'companies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name, password):
+        """initialize with name."""
+        self.name = name
+        self.password = password
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Company.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
