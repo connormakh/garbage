324b332a2a13c8eb76238d75ff231c57d70ab9d4
Connor <connormakhlouta@gmail.com>
2017-10-09 23:25:57 +0300

    add getCompany[needs testing], addCompany[needs checking for duplicates], loginCompany

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 # local import
 from instance.config import app_config
-from routes.Company import router as companyRouter
 
 # initialize sql-alchemy
 db = SQLAlchemy()
-
+from routes.Company import router as company_router
 
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=False)
+
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../instance/config.py')
+
+    app.register_blueprint(company_router, url_prefix="/api/companies")
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    app.register_blueprint(companyRouter, url_prefix="/companies")
-
     return app
--- models/Company.py
+++ models/Company.py
@@ -1,5 +1,9 @@
 from app import db
-
+from instance.config import Config
+from functools import wraps
+from flask import request, jsonify
+import uuid
+import jwt
 
 class Company(db.Model):
     """This Class represents the company table, used for the user type company, in the admin portal"""
@@ -7,29 +11,80 @@ class Company(db.Model):
     __tablename__ = 'companies'
 
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(255))
     password = db.Column(db.String(255))
     email = db.Column(db.String(255))
-    contactNumber = db.Column(db.String(255))
+    contact_number = db.Column(db.String(255))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
 
-    def __init__(self, name, email, password, contactNumber):
+    def __init__(self, name, email, password, contact_number):
         self.name = name
         self.password = password
         self.email = email
-        self.contactNumber = contactNumber
+        self.contact_number  = contact_number
+        self.public_id = str(uuid.uuid4())
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
-    def get_all():
-        return Company.query.all()
+    def get_company(company_id):
+        if id and id != -1:
+            return Company.query.filter_by(id=company_id).first()
+        else:
+            return Company.query.all()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+    @staticmethod
+    def token_required(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = None
+
+            if 'x-access-token' in request.headers:
+                token = request.headers['x-access-token']
+
+            if not token:
+                return jsonify({'message': 'Token is missing!'}), 401
+
+            try:
+                data = jwt.decode(token, Config.SECRET)
+                current_user = Company.query.filter_by(public_id=data['public_id']).first()
+            except:
+                return jsonify({'message': 'Token is invalid!'}), 401
+
+            return f(current_user, *args, **kwargs)
+
+        return decorated
+
+    @staticmethod
+    def authorize_by_name(name, password):
+        current_user = Company.query.filter_by(name=name, password=password).first()
+
+        if current_user:
+            return str(jwt.encode({'public_id': current_user.public_id}, Config.SECRET, algorithm='HS256'))
+
+
+    @staticmethod
+    def authorize_by_email(email, password):
+        current_user = Company.query.filter_by(email=email, password=password).first()
+
+        if current_user:
+            return str(jwt.encode({'public_id': current_user.public_id}, Config.SECRET, algorithm='HS256'))
+
+    def json_serialize(self):
+        return {
+            'id': self.id,
+            'public_id': self.public_id,
+            'name': self.name,
+            'email': self.email,
+            'contact_number': self.contact_number
+        }
